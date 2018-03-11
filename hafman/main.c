493675ef3cf73adfe52a918e97f8fed9ed07e3fb
AlisiaSmit <anya0192837465@ya.ru>
2018-03-11 21:15:01 +0700

    создание

---
+++ hafman/main.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//#include "header.h"
+
+int main(int argc, char *argv[])
+{
+	FILE *in, *out;
+
+	if (!strcmp(argv[1], "-e"))
+	{
+
+	}
+}
